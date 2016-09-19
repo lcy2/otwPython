0af9d878dd29d221ffeb5e69a619104671b5dd40
lcy2 <lichangyi888@hotmail.com>
2016-09-19 04:08:19 +0000

    decrypted the easier things

--- natas28/natas28.py
+++ natas28/natas28.py
@@ -34,12 +34,22 @@ elif test == '2' :
 elif test == '3' :
     temp = '%{0}'
     tar = []
-    for i in range(0, 40):
+    for i in range(12, 100, 16):
         tar.append(temp.format("65") * i)
 
 # custom input
 elif test == '4' :
-    tar = ['bbbbbbbbbbbbb', 'bbbbbbbbbbbcb']
+    tar = ['eeeeeeeeeeC'+'e'*189, 'e'*200]
+
+
+def decodeNatas ( encodedStr ) :
+    R = ['97', '3f', 'd7', '04', 'd2', 'b4', 'a1', 'af', '7a', '52', '98', '38', 'fd', '51', '8c', 'f9']
+    decodedArr = []
+    for i in range(0, len(encodedStr)):
+        decodedArr.append(chr(ord(encodedStr[i]) ^ int(R[i % 16], 16)))
+    return "".join(decodedArr)
+
+
 
 
 
@@ -71,19 +81,50 @@ for i in range(0, len(tar)):
     #print(query)
     #print(tar[i].ljust(3)),
     currstr = base64.b64decode(urllib.unquote(query[1]))
-    #print(currstr)
+    #print '-'*60 
+    #print(decodeNatas(currstr))
+    #print(urllib.unquote(tar[i]))
+    #print '-'* 60
+    
+
     #print(len((base64.b64decode(urllib.unquote(query[1])))))
     
-    # compare the strings
-    for index, bitchar in enumerate(laststr):
-         if bitchar == currstr[index]:
-             sys.stdout.write('_')
-         else:
-             sys.stdout.write('*')
-    sys.stdout.flush()
-    print " ",
-    laststr = base64.b64decode(urllib.unquote(query[1]))
     
- 
+
+
+
+
+    # compare the strings
+    if 1 == 0:
+        for index, bitchar in enumerate(laststr):
+            if bitchar == currstr[index]:
+                sys.stdout.write('_')
+            else:
+                sys.stdout.write('*')
+        sys.stdout.flush()
+        print " ",
+        laststr = base64.b64decode(urllib.unquote(query[1]))
+    
+    # print out the blocks for comparison
+    if 1 == 1:
+        for block in range(0, len(currstr),16):
+            print currstr[block:block+15]
+            if block == 64:
+                crackR = [chr(ord(x) ^ ord("e")) for x in currstr[block:block+16]]
+                print "\n" + "Decrypt" + "\n" + "-" * 60
+                if len(currstr) > 100 :
+                    for chars2 in range(0, len(currstr)):
+                        
+                        sys.stdout.write(chr(ord(currstr[chars2]) ^ ord(crackR[chars2 % 16])))
+                        if chars2 % 16 == 0:
+                            sys.stdout.write("\n")
+                        sys.stdout.flush()
+                    print "\n" + "-" * 70
+                    print crackR
+                    break
+        print '-' * 60
+
+
+    
     #print(urllib.unquote(query[1])),
-    print(urllib.unquote(tar[i]).rjust(2))
+    #print(urllib.unquote(tar[i]).rjust(2))
