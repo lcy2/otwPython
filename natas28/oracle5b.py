7cc336b0a4a1b57436a7e5e81ed38e1924160c07
lcy2 <lichangyi888@hotmail.com>
2016-09-22 03:21:01 +0000

    more oracle5s

--- natas28/oracle5b.py
+++ natas28/oracle5b.py
@@ -9,7 +9,9 @@ import difflib
 # In this oracle, I will repeatedly submit target texts that
 # are smaller and smaller such that i can brute force attack
 # the secret key appended behind the chosen plaintext
-# use percentage for j
+
+# in 5b we use parallel comparison
+# compare the query of bbbb{unknown} to bbbb{known}
 
 
 target = 'http://natas28.natas.labs.overthewire.org/index.php'
@@ -18,7 +20,7 @@ target2 = 'http://natas28.natas.labs.overthewire.org/search.php'
 
 user_auth = ('natas28', 'JWwR438wkgTsNKBbcJoowyysdM82YjeF')
 #tar = string.ascii_letters + string.digits
-pad = 'padpadpadi'
+pad = 'padpadpadp'
 ptx = 'b'
 
 # notes
@@ -33,10 +35,10 @@ header = {'Content-Type': 'application/x-www-form-urlencoded'}
 
 
 
-secretkey = ''
+secretkey = '%25'
 
 
-for i in range(1, 33):
+for i in range(2, 33):
     #tar = pad + ptx * (64 - i) + secretkey
     #payload = 'query=%s' % tar
     #response = requests.post(target, auth = user_auth, headers = header, data = payload, allow_redirects = True)
@@ -54,13 +56,19 @@ for i in range(1, 33):
     # experimental block is from [80:112]
     # plaintext block is from [48:80]
     
-    for j in range(0, 256):
+    for j in range(0,256):
+        print "-" * 60
 
-        print "-" * 60            
-        tar2 = pad + ptx * (32 - i) + secretkey +\
-            "%" + format(j,'x').zfill(2) + ptx * (32 - i)
+        if chr(j) == "&":
+            continue
 
-        print "lenghth of tar2: " + str(len(tar2)) + " i = " + str(i)
+        # this is the known query
+        tar2 = pad + ptx * 16 + ptx * (16 - i) + secretkey + "%" + format(j, 'x').zfill(2)
+        print tar2, len(tar2) - 10
+        # this is the unknown one
+        tar3 = pad + ptx * (32 - i)
+        print tar3, len(tar3) - 10
+        #print "lenghth of tar2: " + str(len(tar2)) + " i = " + str(i)
         
         payload2 = 'query=%s' % tar2
         response2 = requests.post(target, auth = user_auth, headers = header, data = payload2, allow_redirects = True)
@@ -68,11 +76,19 @@ for i in range(1, 33):
         
         urlgetvar2 = base64.b64decode(urllib.unquote(query2[1]))
         bytesarray2 = [ord(x) for x in urlgetvar2]
+        
+        
+        payload3 = 'query=%s' % tar3
+        response3 = requests.post(target, auth = user_auth, headers = header, data = payload3, allow_redirects = True)
+        query3 = str.split(response3.url.encode('ascii','ignore'), '=')
+        
+        urlgetvar3 = base64.b64decode(urllib.unquote(query3[1]))
+        bytesarray3 = [ord(x) for x in urlgetvar3]
         #print "".join([format(x, 'x').zfill(2) for x in bytesarray2[48:80]]),
         #print " ",
         #print "".join([format(x, 'x').zfill(2) for x in bytesarray2[80:112]]),
         
-        if bytesarray2[48:64] == bytesarray2[80:96] :
+        if bytesarray2[48:64] == bytesarray2[48:64] :
             print "1st block checks out"
         
         
@@ -81,12 +97,15 @@ for i in range(1, 33):
 
 
         tmptxt = ''
+        tmptxt2 = ''
         for index, eachchar in enumerate(bytesarray2):
             tmptxt += format(eachchar, 'x').zfill(2)
+            tmptxt2 += format(bytesarray3[index], 'x').zfill(2)
             if index % 16 == 15:
-                print tmptxt
+                print tmptxt, tmptxt2
                 tmptxt = ''
-
+                tmptxt2 = ''
+        print len(bytesarray2), len(bytesarray3)
 #        a = "".join([format(x, 'x').zfill(2) for x in bytesarray2[64:80]])
 #        b = "".join([format(x, 'x').zfill(2) for x in bytesarray2[96:112]])
  
@@ -109,7 +128,7 @@ for i in range(1, 33):
         #    twochars += chr(j) + " " + str(j) + "\n"
             
 
-        if bytesarray2[48:80] == bytesarray2[80:112] :
+        if bytesarray2[48:80] == bytesarray3[48:80] :
             secretkey += chr(j)
             print "breaking"
             break
--- natas28/oracle5c.py
+++ natas28/oracle5c.py
@@ -9,7 +9,10 @@ import difflib
 # In this oracle, I will repeatedly submit target texts that
 # are smaller and smaller such that i can brute force attack
 # the secret key appended behind the chosen plaintext
-# use percentage for j
+
+# in 5c we use parallel comparison + SN2
+# assuming the blocks are padded correctly
+# make it such that the last block = {unknown} + padding
 
 
 target = 'http://natas28.natas.labs.overthewire.org/index.php'
@@ -18,7 +21,7 @@ target2 = 'http://natas28.natas.labs.overthewire.org/search.php'
 
 user_auth = ('natas28', 'JWwR438wkgTsNKBbcJoowyysdM82YjeF')
 #tar = string.ascii_letters + string.digits
-pad = 'padpadpadi'
+pad = 'padpadpadp'
 ptx = 'b'
 
 # notes
@@ -32,9 +35,8 @@ header = {'Content-Type': 'application/x-www-form-urlencoded'}
 
 
 
-
+secretkey = ';3'
 secretkey = ''
-
 
 for i in range(1, 33):
     #tar = pad + ptx * (64 - i) + secretkey
@@ -54,13 +56,19 @@ for i in range(1, 33):
     # experimental block is from [80:112]
     # plaintext block is from [48:80]
     
-    for j in range(0, 256):
+    for j in range(31,127):
+        print "-" * 60
 
-        print "-" * 60            
-        tar2 = pad + ptx * (32 - i) + secretkey +\
-            "%" + format(j,'x').zfill(2) + ptx * (32 - i)
+        if chr(j) == "&":
+            continue
 
-        print "lenghth of tar2: " + str(len(tar2)) + " i = " + str(i)
+        # this is the known query / guessing query
+        tar2 = pad + chr(j) + secretkey[::-1] + "%{0}".format(format(16-i, "x").zfill(2)) * (16 - i) + ptx * 3
+        print tar2, len(tar2), (len(tar2) + 32 + 19 + 16)
+        # this is the unknown one
+        tar3 = pad + ptx * (16 + 3 + i)
+        print tar3, len(tar3), len(tar3) + 32 + 19 + 16
+        #print "lenghth of tar2: " + str(len(tar2)) + " i = " + str(i)
         
         payload2 = 'query=%s' % tar2
         response2 = requests.post(target, auth = user_auth, headers = header, data = payload2, allow_redirects = True)
@@ -68,25 +76,34 @@ for i in range(1, 33):
         
         urlgetvar2 = base64.b64decode(urllib.unquote(query2[1]))
         bytesarray2 = [ord(x) for x in urlgetvar2]
+        
+        
+        payload3 = 'query=%s' % tar3
+        response3 = requests.post(target, auth = user_auth, headers = header, data = payload3, allow_redirects = True)
+        query3 = str.split(response3.url.encode('ascii','ignore'), '=')
+        
+        urlgetvar3 = base64.b64decode(urllib.unquote(query3[1]))
+        bytesarray3 = [ord(x) for x in urlgetvar3]
         #print "".join([format(x, 'x').zfill(2) for x in bytesarray2[48:80]]),
         #print " ",
         #print "".join([format(x, 'x').zfill(2) for x in bytesarray2[80:112]]),
         
-        if bytesarray2[48:64] == bytesarray2[80:96] :
-            print "1st block checks out"
         
         
-        print " " + format(j,'x').zfill(2) + " " + secretkey + " " + chr(j)
+        print " " + format(j,'x').zfill(2) + " " + secretkey[::-1] + " " + chr(j)
         
 
 
+        print len(bytesarray2), len(bytesarray3)
         tmptxt = ''
+        tmptxt2 = ''
         for index, eachchar in enumerate(bytesarray2):
             tmptxt += format(eachchar, 'x').zfill(2)
+            tmptxt2 += format(bytesarray3[index], 'x').zfill(2)
             if index % 16 == 15:
-                print tmptxt
+                print tmptxt, tmptxt2
                 tmptxt = ''
-
+                tmptxt2 = ''
 #        a = "".join([format(x, 'x').zfill(2) for x in bytesarray2[64:80]])
 #        b = "".join([format(x, 'x').zfill(2) for x in bytesarray2[96:112]])
  
@@ -108,8 +125,11 @@ for i in range(1, 33):
         #    print "Two char line?"
         #    twochars += chr(j) + " " + str(j) + "\n"
             
+        print "".join([format(x, 'x').zfill(2) for x in bytesarray2[48:64]]),
+        print " >> ",
+        print "".join([format(x, 'x').zfill(2) for x in bytesarray3[96:112]])
 
-        if bytesarray2[48:80] == bytesarray2[80:112] :
+        if bytesarray2[48:64] == bytesarray3[96:112] :
             secretkey += chr(j)
             print "breaking"
             break
